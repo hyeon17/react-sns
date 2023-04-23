ba5e400dc09fb16e9bb79befd036890a0cded4d5
김민기 <kimminki.dev@gmail.com>
2023-04-24 05:18:26 +0900

    feat: UserInfo 영역 요소 추가 및 스타일 적용

--- src/components/User/UserInfo.tsx
+++ src/components/User/UserInfo.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import * as S from './style'
+import axios from 'axios'
+
+
 
 function UserInfo() {
+
+  // const getPosts = async () => {
+  //   const response = await axios.get(import.meta.env.VITE_SERVER_URL + '/posts')
+  //   console.log(response)
+  // }
+  const imgSrc:string = "https://picsum.photos/150/150"
+  const userName:string = "유저 네임"
+  const userEmail:string = "test@gamil.com"
+  const postCount:string = "123"
   return (
-    <div>UserInfo</div>
+    <S.gridDiv>
+      <img src={imgSrc} />
+      <div>{userName}</div>
+      <div>게시물 <span>{postCount}</span></div>
+      <div>{userEmail}</div>
+    </S.gridDiv>
   )
 }
 
--- src/components/User/style.ts
+++ src/components/User/style.ts
@@ -1,2 +1,22 @@
 import styled from "styled-components";
 import { theme } from '../../styles/theme'
+
+export const gridDiv = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  img {
+    grid-column: 1/2;
+    grid-row: 1/4;
+    border-radius: 50%;
+    justify-self: center;
+  }
+  width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+  border-bottom: 1px solid ${theme.colors.dimGray};
+  span {
+    font-weight: bold;
+  }
+`

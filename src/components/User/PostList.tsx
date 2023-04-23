065ddd8beff63c8ae18ed4b63d7b2a75a3f9415e
김민기 <kimminki.dev@gmail.com>
2023-04-24 03:06:22 +0900

    feat: Create files for UserPage
    - UserInfo.tsx
    - PostList.tsx
    - style.ts

---
+++ src/components/User/PostList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './style'
+
+function PostList() {
+  return (
+    <div>PostList</div>
+  )
+}
+
+export default PostList
---
+++ src/components/User/UserInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './style'
+
+function UserInfo() {
+  return (
+    <div>UserInfo</div>
+  )
+}
+
+export default UserInfo

b0f885bba6d0a6c03466d04ff9c62e399cee8984
JangHyeonJun <janghyeon8725@naver.com>
2023-04-25 00:18:42 +0900

    fix: 글 등록 영역 코드 분리

--- src/components/PostModal/index.tsx
+++ src/components/PostModal/index.tsx
@@ -1,31 +1,23 @@
 import { modalType } from '@/types/modal';
-import { FaPhotoVideo } from 'react-icons/fa';
 import { useStore } from '../../store/store';
-
 import * as S from './style';
+import { Helmet } from 'react-helmet-async';
 
-function PostModal() {
-
+function PostModal({ content }: modalType) {
   const { closeModal } = useStore();
 
   return (
     <S.ModalWrapper>
+      <Helmet>
+        <title>새 게시물 만들기 · Photogram </title>
+      </Helmet>
       <S.Content>
-        <S.CloseButton onClick={closeModal} />
         <S.Header>
+          <S.CloseButton onClick={closeModal} />
           <S.Title>새 게시물 만들기</S.Title>
-          <S.PostButton>게시 버튼</S.PostButton>
+          <S.PostButton>공유하기</S.PostButton>
         </S.Header>
-        <div>
-          <div>
-            <FaPhotoVideo />
-            <p>사진과 동영상을 여기에 끌어다 놓으세요</p>
-            <button>사진/동영상 추가</button>
-          </div>
-          <div>
-            <div>게시물에 대해 설명해보세요...</div>
-          </div>
-        </div>
+        {content}
       </S.Content>
     </S.ModalWrapper>
   );

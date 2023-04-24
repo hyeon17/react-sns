abe334d5328bd90ec4b12cb977ee59d73cd8fece
김민기 <kimminki.dev@gmail.com>
2023-04-24 11:35:26 +0900

    test: 유저 페이지 테스트용 데이터와 페이지 추가
    - testData.ts
    - 일단 테스트 페이지에 컴포넌트들을 추가해 놓았습니다.

--- src/components/ComponentTest.tsx
+++ src/components/ComponentTest.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import UserInfo from "@/components/User/UserInfo";
+import PostList from "@/components/User/PostList";
 
 interface ComponentTestProps {
   element?: React.ReactNode;
 }
 
 function ComponentTest({ element }: ComponentTestProps) {
-  return <>{element ?? null}</>;
+  return (
+    <>{element ?? null}
+      <UserInfo />
+      <PostList />
+    </>
+  );
 }
 
 export default ComponentTest;

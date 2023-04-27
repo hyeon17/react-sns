7e31afe1e7c6e85ce82cca4a8d227529dbb80e3b
김민기 <kimminki.dev@gmail.com>
2023-04-28 03:33:15 +0900

    feat: user페이지에서 프로필 이미지에 대체이미지 적용

--- src/components/User/UserInfo.tsx
+++ src/components/User/UserInfo.tsx
@@ -1,13 +1,16 @@
 import * as S from './style'
 import { profile } from "@/types/user"
-
+import { CgProfile } from "react-icons/cg"
+import { theme } from '../../styles/theme'
 
 function UserInfo({ userProfile, postLength }: { userProfile: profile | undefined, postLength: number } ) {
   
   return (
     <S.gridDiv>
       <div className='imgWrapper'>
-        {/* <img src={userProfile?.profile} /> */}
+        {userProfile?.profile ? 
+        <img src={userProfile?.profile} /> : 
+        <CgProfile size={150} color={theme.colors.dimGray} />}
       </div>
       <div>{userProfile?.username}</div>
       <div>게시물 <span>{postLength}</span></div>

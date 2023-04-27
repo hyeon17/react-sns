ac5bab0d42cefd93a42c30a3ef9d7d5815663d63
JangHyeonJun <janghyeon8725@naver.com>
2023-04-27 23:06:18 +0900

    feat: 글 등록 api 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,1 +1,18 @@
+import { instance } from '@/api/axios';
+import { createBearerToken } from '@/util';
+import { CreatePostRequest } from '../types/request';
+import { PostForm } from '@/types/modal';
 
+export const postMutation = async ({ content, files }: PostForm) => {
+  const res = await instance.post(
+    '/posts',
+    { content, files },
+    {
+      headers: {
+        // Authorization: createBearerToken(''),
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+  return res;
+};

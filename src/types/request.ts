a75b17ecc1c58d41ada8474e210a52e02f69a402
JangHyeonJun <janghyeon8725@naver.com>
2023-04-30 07:48:59 +0900

    feat: CreateCommentRequest에 id 형식 추가

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,19 +1,19 @@
-import { Comment, Post, User } from "./entity";
+import { Comment, Post, User } from './entity';
 
-export type LoginRequest = Pick<User, "email" | "password">;
+export type LoginRequest = Pick<User, 'email' | 'password'>;
 
-export type SignupRequest = Pick<User, "email" | "password" | "username"> & {
+export type SignupRequest = Pick<User, 'email' | 'password' | 'username'> & {
   profile?: File;
 };
 
-export type CreatePostRequest = Pick<Post, "content"> & {
+export type CreatePostRequest = Pick<Post, 'content'> & {
   files: File[];
 };
 
-export type UpdatePostRequest = Pick<Post, "content"> & {
+export type UpdatePostRequest = Pick<Post, 'content'> & {
   files: File[];
 };
 
-export type CreateCommentRequest = Pick<Comment, "content">;
+export type CreateCommentRequest = Pick<Comment, 'id' | 'content'>;
 
-export type UpdateCommentRequest = Pick<Comment, "content">;
+export type UpdateCommentRequest = Pick<Comment, 'content'>;
